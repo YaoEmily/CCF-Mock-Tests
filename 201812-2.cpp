dd5b709fd6cf0e07c7b880473f0b53dad41f6769
YaoEmily <xuheyao_emily@163.com>
2019-03-04 16:14:47 +0800

    Update 201812-2.cpp

--- 201812-2.cpp
+++ 201812-2.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main(void)
-{
+int main(void){
     int r, y, g;
     cin>>r>>y>>g;
     int n;
     cin>>n;
     long long sum = 0;
     long long x1 = y, x2 = r+y, x3 = r+y+g;
-    while(n)
-    {
+    while(n){
         n--;
         int k, t;
         cin>>k>>t;
-        if(k==0)
-            sum+=t;
-        else
-        {
+        if(k==0) sum += t;
+        else{
             long long tmp;
-            if(k==1)//ºìµÆ
-                tmp = (r-t+sum+y)%x3;
-            else if(k==2)//»ÆµÆ
-                tmp = (y-t+sum)%x3;
-            else//ÂÌµÆ
-                tmp = (g-t+sum+r+y)%x3;
-            if(tmp<x1)
-            {
+            if(k==1) tmp = (r-t+sum+y)%x3; //çº¢ç¯
+            else if(k==2) tmp = (y-t+sum)%x3; //é»„ç¯
+            else tmp = (g-t+sum+r+y)%x3; //ç»¿ç¯
+            if(tmp<x1){
                 sum += y+r-tmp;
             }
-            else if(tmp<x2)
-            {
+            else if(tmp<x2){
                 sum = sum+y+r-tmp;
             }
         }
